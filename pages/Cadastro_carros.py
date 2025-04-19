b0312debfbc229476bd1351197102774f0f518ad
Jeferson Souza <mp_bra_nco@hotmail.com>
2025-04-19 01:39:22 -0300

    Calculo de combustível
    Ainda nascendo!

---
+++ pages/Cadastro_carros.py
@@ -0,0 +1,66 @@
+import locale
+import streamlit as st
+import datetime
+
+#--------------------------------------------------------------
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+st.set_page_config(page_title='Cadastro', initial_sidebar_state='collapsed')
+#--------------------------------------------------------------
+
+
+st.markdown('### Cadastro do preço de combustivel atual ###')
+col_comb01, col_comb02 = st.columns(2)
+with col_comb01:
+    preco_gasolina = st.number_input('Valor da Gasolina', placeholder='7,555')
+    ult_vlr_gasolina = 5.79 # retornar do banco de dados
+    ult_vlr_gasolinaf = locale.format_string("%.2f", ult_vlr_gasolina, grouping=True)    
+    st.metric(label='Ultimo valor', value=ult_vlr_gasolinaf)
+    
+
+with col_comb02:
+    preco_etanol = st.number_input('Valor do **Etanol**', placeholder='7,555')
+    ult_vlr_etanol = 5.19 # retornar do banco de dados
+    ult_vlr_etanolf = locale.format_string("%.2f", ult_vlr_etanol, grouping=True)    
+    st.metric(label='Ultimo valor', value=ult_vlr_etanolf)    
+
+
+if st.button("Salvar Valores", key='bt_add_preco'):
+    # Aqui você pode adicionar a lógica para adicionar a despesa ao banco de dados
+    st.success("Despesa adicionada com sucesso!")
+    
+
+#--------------------------------------------------------------
+st.divider()
+#--------------------------------------------------------------
+
+st.markdown('### Cadastro de carros ###')
+
+col01, col02  = st.columns(2)
+with col01:
+    modelo_carro = st.text_input('Informe o nome/modelo do Carro', placeholder='Ford KA')
+    ano_carro = st.number_input('Ano do Carro', placeholder='2025', min_value=1900)
+
+with col02:    
+    placa = st.text_input('Placa do carro', placeholder='ABC 1234')
+    km_listro = st.number_input('Quilometros por litro (Km/L)', placeholder='9,8')
+    
+
+#--------------------------------------------------------------
+col03, col04 = st.columns(2, border=True)
+with col03:
+    st.write('Autonomia do tanque')
+    total_litros = 2
+    autonomia_tanque = 5
+
+with col04:
+    st.markdown('Litros no tanque')
+
+if st.button("Salvar Valores", key='bt_add_carr'):
+    # Aqui você pode adicionar a lógica para adicionar a despesa ao banco de dados
+    st.success("Despesa adicionada com sucesso!")
+
+st.divider()
+st.dataframe()
+
+
+
